e32cf36fcd21be94e043c6ae419e3356f62fed3a
Youxi Pan <130309628+MGNub@users.noreply.github.com>
2024-09-23 01:46:09 -0400

    Update commute.js

--- JAVASCRIPT/commute.js
+++ JAVASCRIPT/commute.js
@@ -1,10 +1,6 @@
-// window.scrollTo(0, 0);
+window.scrollTo(0, 0);
 setTimeout(showVideo, 850);
 setTimeout(letScroll, 2000);
-
-// $(function() {
-//        jQuery.scrollSpeed(100, 20, 'ease-in');
-// });
 
 const topVideo = document.getElementById('topVideo');
 // let theme = document.documentElement.style.getPropertyValue('--bgColor');
@@ -24,6 +20,8 @@ function parallax() {
     const transitVideo = document.getElementById('transitVideo');
     const trafficImg = document.getElementById('trafficImg');
     const bikeHorizontal = document.getElementById('bikeHorizontal');
+
+    // console.log(window.scrollY);
     
     if(window.scrollY > 0) {
         document.getElementById('title').classList.add('fade');
@@ -33,7 +31,7 @@ function parallax() {
         logo.classList.remove('rotate');
     }
 
-    if (window.scrollY > window.innerHeight * 7.4 + window.innerWidth * 4) {
+    if (window.scrollY > window.innerHeight * 6.9 + window.innerWidth * 4) {
         bikeVideo.style.display = 'none';
         transitVideo.style.display = 'block';
     } else if(window.scrollY > window.innerHeight) {
@@ -45,24 +43,31 @@ function parallax() {
         bikeVideo.style.display = 'none';
     }
 
-    if (window.scrollY > window.innerHeight * 3.2 && window.scrollY < window.innerHeight * 4.7) {
-        trafficImg.style.left = window.innerWidth + (window.innerHeight/1.2) - window.innerHeight * 1.6;
-        trafficImg.style.marginLeft = window.pageYOffset - (window.innerHeight * 3.2);
-        console.log('hey');
+    if (screen.width > 600) {
+        if (window.scrollY > window.innerHeight * 3.2 && window.scrollY < window.innerHeight * 4.7) {
+            trafficImg.style.left = window.innerWidth + (window.innerHeight/1.2) - window.innerHeight * 1.6;
+            trafficImg.style.marginLeft = window.pageYOffset - (window.innerHeight * 3.2);
+        }
+        if (window.scrollY < window.innerHeight * 1.6) {
+            trafficImg.style.left = window.innerWidth + (window.innerHeight/1.2) - window.pageYOffset;
+        } else if (window.scrollY < window.innerHeight * 3.2) {
+            // trafficImg.style.transform = 'translateX(0vw) translateY(-25%)';
+            // trafficImg.style.opacity = 1;
+        } else if (window.scrollY > window.innerHeight * 5.3) {
+            bikeHorizontal.style.marginLeft = (window.innerHeight * 6.4) + (-1 * window.pageYOffset);
+        } else if (window.scrollY > window.innerHeight * 4.6) {
+            document.getElementById('page2').style.background = 'rgb(117, 156, 212)'; //rgb(153, 185, 231)
+        } 
+    } else {
+        if (window.scrollY > window.innerHeight * 4.42) {
+            bikeHorizontal.style.marginLeft = (window.innerHeight * 4.42) + (-1 * window.pageYOffset);
+        } else {
+            bikeHorizontal.style.marginLeft = (window.innerHeight * 4.42);
+        }
     }
-    if (window.scrollY < window.innerHeight * 1.6) {
-        trafficImg.style.left = window.innerWidth + (window.innerHeight/1.2) - window.pageYOffset;
-        console.log(window.pageYOffset);
 
-    } else if (window.scrollY < window.innerHeight * 3.2) {
-        // trafficImg.style.transform = 'translateX(0vw) translateY(-25%)';
-        // trafficImg.style.opacity = 1;
-    } else if (window.scrollY > window.innerHeight * 5.3) {
-        bikeHorizontal.style.marginLeft = (window.innerHeight * 6.4) + (-1 * window.pageYOffset);
-    } else if (window.scrollY > window.innerHeight * 4.6) {
-        document.getElementById('page2').style.background = 'rgb(117, 156, 212)'; //rgb(153, 185, 231)
-        document.getElementById('page2Text').style.color = 'white';
-    } 
+    
+
     // else if (window.scrollY > window.innerHeight * 3.2) {
     //     trafficImg.style.left = -1 * window.pageYOffset - window.innerHeight * 3.2;
     //     console.log('hey');
@@ -80,46 +85,105 @@ function parallax() {
         scrollProgress.style.transform = 'translate(0px, -50%)';
     }
     
-    if (window.scrollY > window.innerHeight * 12 + window.innerWidth) {
-        scrollProgress.style.opacity = 0;
-        scrollProgress.style.transform = 'translate(-50px, -50%)';
-    } else if (window.scrollY > window.innerHeight * 10.8 + window.innerWidth) {
-        circles[1].style.background = 'rgb(44, 44, 44)';
-        circles[2].style.background = 'rgb(218, 218, 218)';
-        bikeText.innerHTML = "<h2>Stigma</h1><p>In the states, there is a stigma surrounding cyclists. Since American roads aren't designed for bicycles, and bicycles and cars have to share the same road, drivers and cyclists often get in each other's way, and the media often exaggerates events, elevating the tension even more. A lot of people would assume that anyone riding a bike is doing so for sports-related purposes. If the infrastructure for bicycles weren't basically non-existent, there would definitely be less feud between drivers and cycists.</p>";
-        bikeText.classList.remove('bikeHidden');
-    } else if (window.scrollY > window.innerHeight * 10.5 + window.innerWidth) {
-        bikeText.classList.add('bikeHidden');
-    } else if (window.scrollY > window.innerHeight * 9.8 + window.innerWidth) {
-        circles[0].style.background = 'rgb(44, 44, 44)';
-        circles[1].style.background = 'rgb(218, 218, 218)';
-        circles[2].style.background = 'rgb(44, 44, 44)';
-        bikeText.innerHTML = '<h2>Commute Trouble</h2><p>While traveling by foot and bicycles are great ways to commute, there are difficulties that come with it depending on the city or area. For example, some cities have a lot of wide stroads that are dangerous to walk across or bike along. Lack of bike lanes in some cities also makes it more difficult and inconvenient for both drivers and bikers. Both pedestrian and bike infrastructure in the US need a serious reform.</p>'; //deterr people from biking or using trains (conclusion after both bike and train.)
-        bikeText.classList.remove('bikeHidden');
-    } else if (window.scrollY > window.innerHeight * 9.5 + window.innerWidth) {
-        bikeText.classList.add('bikeHidden');
-    } else if (window.scrollY > window.innerHeight * 8.8 + window.innerWidth) {
-        circles[0].style.background = 'rgb(218, 218, 218)';
-        circles[1].style.background = 'rgb(44, 44, 44)';
-        scrollProgress.style.opacity = 1;
-        scrollProgress.style.transform = 'translate(0, -50%)';
-        bikeText.innerHTML = '<h2>Walking</h2><p>Traveling by foot is definitely one of the most reliable and healthiest way to commute shorter distances. It allows for an individual to exercise while moving towards their destination at the same time.</p><h2>Biking</h2><p>Along with walking, using a bicycle is also an excellent method of commuting. There are many cities that allow bikes to be rented for a small price or for free, which makes it more accessible, and it is also significantly faster than going on foot, making it a good option for medium and longer distances.</p>';
-        bikeText.classList.remove('bikeHidden');
+    if (screen.width > 600) {
+        if (window.scrollY > window.innerHeight * 12 + window.innerWidth) {
+            scrollProgress.style.opacity = 0;
+            scrollProgress.style.transform = 'translate(-50px, -50%)';
+        } else if (window.scrollY > window.innerHeight * 10.8 + window.innerWidth) {
+            circles[1].style.background = 'rgb(44, 44, 44)';
+            circles[2].style.background = 'rgb(218, 218, 218)';
+            bikeText.innerHTML = "<h2>Stigma</h1><p>In the states, there is a stigma surrounding cyclists. Since American roads aren't designed for bicycles, and bicycles and cars have to share the same road, drivers and cyclists often get in each other's way, and the media often exaggerates events, elevating the tension even more. A lot of people would assume that anyone riding a bike is doing so for sports-related purposes. If the infrastructure for bicycles weren't basically non-existent, there would definitely be less feud between drivers and cycists.</p>";
+            bikeText.classList.remove('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 10.5 + window.innerWidth) {
+            bikeText.classList.add('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 9.8 + window.innerWidth) {
+            circles[0].style.background = 'rgb(44, 44, 44)';
+            circles[1].style.background = 'rgb(218, 218, 218)';
+            circles[2].style.background = 'rgb(44, 44, 44)';
+            bikeText.innerHTML = '<h2>Commute Trouble</h2><p>While traveling by foot and bicycles are great ways to commute, there are difficulties that come with it depending on the city or area. For example, some cities have a lot of wide stroads that are dangerous to walk across or bike along. Lack of bike lanes in some cities also makes it more difficult and inconvenient for both drivers and bikers. Both pedestrian and bike infrastructure in the US need a serious reform.</p>'; //deterr people from biking or using trains (conclusion after both bike and train.)
+            bikeText.classList.remove('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 9.5 + window.innerWidth) {
+            bikeText.classList.add('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 8.8 + window.innerWidth) {
+            circles[0].style.background = 'rgb(218, 218, 218)';
+            circles[1].style.background = 'rgb(44, 44, 44)';
+            scrollProgress.style.opacity = 1;
+            scrollProgress.style.transform = 'translate(0, -50%)';
+            bikeText.innerHTML = '<h2>Walking</h2><p>Traveling by foot is definitely one of the most reliable and healthiest way to commute shorter distances. It allows for an individual to exercise while moving towards their destination at the same time.</p><h2>Biking</h2><p>Along with walking, using a bicycle is also an excellent method of commuting. There are many cities that allow bikes to be rented for a small price or for free, which makes it more accessible, and it is also significantly faster than going on foot, making it a good option for medium and longer distances.</p>';
+            bikeText.classList.remove('bikeHidden');
+        } else {
+            scrollProgress.style.opacity = 0;
+            scrollProgress.style.transform = 'translate(-50px, -50%)';
+            bikeText.classList.remove('bikeHidden');
+        }
     } else {
-        scrollProgress.style.opacity = 0;
-        scrollProgress.style.transform = 'translate(-50px, -50%)';
-        bikeText.classList.remove('bikeHidden');
+        if (window.scrollY > window.innerHeight * 6.2 + window.innerWidth) {
+            scrollProgress.style.opacity = 0;
+            scrollProgress.style.transform = 'translate(-50px, -50%)';
+        } else if (window.scrollY > window.innerHeight * 5.7 + window.innerWidth) {
+            circles[1].style.background = 'rgb(44, 44, 44)';
+            circles[2].style.background = 'rgb(218, 218, 218)';
+            bikeText.innerHTML = "<h2>Stigma</h1><p>In the states, there is a stigma surrounding cyclists. Since American roads aren't designed for bicycles, and bicycles and cars have to share the same road, drivers and cyclists often get in each other's way, and the media often exaggerates events, elevating the tension even more. A lot of people would assume that anyone riding a bike is doing so for sports-related purposes. If the infrastructure for bicycles weren't basically non-existent, there would definitely be less feud between drivers and cycists.</p>";
+            bikeText.classList.remove('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 5.6 + window.innerWidth) {
+            bikeText.classList.add('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 5.1 + window.innerWidth) {
+            circles[0].style.background = 'rgb(44, 44, 44)';
+            circles[1].style.background = 'rgb(218, 218, 218)';
+            circles[2].style.background = 'rgb(44, 44, 44)';
+            bikeText.innerHTML = '<h2>Commute Trouble</h2><p>While traveling by foot and bicycles are great ways to commute, there are difficulties that come with it depending on the city or area. For example, some cities have a lot of wide stroads that are dangerous to walk across or bike along. Lack of bike lanes in some cities also makes it more difficult and inconvenient for both drivers and bikers. Both pedestrian and bike infrastructure in the US need a serious reform.</p>'; //deterr people from biking or using trains (conclusion after both bike and train.)
+            bikeText.classList.remove('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 5 + window.innerWidth) {
+            bikeText.classList.add('bikeHidden');
+        } else if (window.scrollY > window.innerHeight * 4.5 + window.innerWidth) {
+            circles[0].style.background = 'rgb(218, 218, 218)';
+            circles[1].style.background = 'rgb(44, 44, 44)';
+            scrollProgress.style.opacity = 1;
+            scrollProgress.style.transform = 'translate(0, -50%)';
+            bikeText.innerHTML = '<h2>Walking</h2><p>Traveling by foot is definitely one of the most reliable and healthiest way to commute shorter distances. It allows for an individual to exercise while moving towards their destination at the same time.</p><h2>Biking</h2><p>Along with walking, using a bicycle is also an excellent method of commuting. There are many cities that allow bikes to be rented for a small price or for free, which makes it more accessible, and it is also significantly faster than going on foot, making it a good option for medium and longer distances.</p>';
+            bikeText.classList.remove('bikeHidden');
+        } else {
+            scrollProgress.style.opacity = 0;
+            scrollProgress.style.transform = 'translate(-50px, -50%)';
+            bikeText.classList.remove('bikeHidden');
+        }
     }
     //transitText
-    const texts = document.getElementsByClassName('transitText');
+    
+    if (screen.width > 600) {
+        fadeScroll(0, 0);
+        fadeScroll(1, 1.75);
+        fadeScroll(2, 3.5);
+    } else {
+        fadeScroll(0, 50);
+        fadeScroll(1, 45);
+        fadeScroll(2, 40);
+    }
+    
+    function fadeScroll(index, init) {
+        texts[index].style.opacity = 21.1 - ((window.pageYOffset / maxOffset(offset[index])) * 20);
 
-    fadeScroll(0, 30);
-    fadeScroll(1, 28);
-    fadeScroll(2, 26);
-    function fadeScroll(text, init) {
-        texts[text].style.opacity = window.innerHeight / init - window.pageYOffset / 550;
+
+        // function slowOffset(after) {
+        //     if (window.pageYOffset > after) {
+        //         return window.pageYOffset / 50;
+        //     }
+        //     return (offset[index] * -1) - wa;
+        // }
+    }
+    console.log(12 - ((window.pageYOffset / maxOffset(offset[0])) * 11));
+
+    function maxOffset(maximum) {
+        if(window.pageYOffset > maximum) {
+            return maximum;
+        }
+        return window.pageYOffset;
     }
 }
+
+const bodyOffset = document.body.getBoundingClientRect().top;
+const texts = document.getElementsByClassName('transitText');
+const offset = [texts[0].getBoundingClientRect().top - bodyOffset, texts[1].getBoundingClientRect().top - bodyOffset, texts[2].getBoundingClientRect().top - bodyOffset];
 
 function bikeToggle() {
     const bikeText = document.querySelector('.bike:nth-child(3) .text');
