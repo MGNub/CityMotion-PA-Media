cf0e69271c0a69de320e182f924269b1889df7a8
Youxi Pan <130309628+MGNub@users.noreply.github.com>
2024-09-23 01:45:07 -0400

    Update home.js

--- JAVASCRIPT/home.js
+++ JAVASCRIPT/home.js
@@ -1,12 +1,23 @@
 window.scrollTo(0, 0);
 setTimeout(letScroll, 4000);
+setTimeout(enableParallax, 7000);
 
 function letScroll() {
-    document.querySelector('body').style.overflowY = 'scroll';
+    if (screen.width > 600) {
+        document.querySelector('body').style.overflowY = 'scroll';
+    }
+    
+}
+
+function enableParallax() {
+     document.getElementById('menu').style.pointerEvents = "all";
+     document.querySelector('main').style.pointerEvents = "all";
+     document.getElementById('descendBtn').style.pointerEvents = "all";
+     parallaxItems.forEach((el) => {el.style.transition = "0.5s cubic-bezier(.2, .49, .32, .99)"});
 }
 
 
-const pageHeight = window.innerHeight;
+const aspectRatio = 1600 * (window.innerHeight / window.innerWidth);
 
 function parallax() {
     const card1 = document.getElementById('card1');
@@ -21,21 +32,21 @@ function parallax() {
         logo.classList.remove('rotate');
     }
 
-    if (scrollY < 730 * 3.2) {
-        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (window.pageYOffset / 10 - 730 / 5) + 'px)';
+    if (scrollY < aspectRatio * 3.2) {
+        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (window.pageYOffset / 15.4 - aspectRatio / 5.25) + 'vh)';
     }
-    else if (scrollY < 730 * 4) {
-        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (730 * 3.2 / 10 - 730 / 5) + 'px)';
-        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((window.pageYOffset / 22.5) - 730 / 7.5) + 'px)';
+    else if (scrollY < aspectRatio * 4.2) {
+        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (aspectRatio * 3.2 / 15.4 - aspectRatio / 5.25) + 'vh)';
+        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((window.pageYOffset / 22.5) - aspectRatio / 7) + 'px)';
         card2.style.opacity = -8 + window.pageYOffset / 320;
-        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((window.pageYOffset / 22.5) - 730 / 7.5) + 'px)';
+        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((window.pageYOffset / 22.5) - aspectRatio / 7) + 'px)';
         text2.style.opacity = -8 + window.pageYOffset / 320;
     } else {
-        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (730 * 3.2 / 10 - 730 / 5) + 'px)'; //try converting px to % or smthin.
-        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((730 * 4 / 22.5) - 730 / 7.5) + 'px)';
-        card2.style.opacity = -8 + 730 * 4 / 320;
-        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((730 * 4 / 22.5) - 730 / 7.5) + 'px)';
-        text2.style.opacity = -8 + 730 * 4 / 320;
+        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (aspectRatio * 3.2 / 15.4 - aspectRatio / 5.25) + 'vh)'; //try converting px to % or smthin.
+        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((aspectRatio * 4.2 / 22.5) - aspectRatio / 7) + 'px)';
+        card2.style.opacity = -8 + aspectRatio * 4.2 / 320;
+        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((aspectRatio * 4.2 / 22.5) - aspectRatio / 7) + 'px)';
+        text2.style.opacity = -8 + aspectRatio * 4.2 / 320;
     }
 }
 
@@ -46,15 +57,20 @@ parallax();
 console.log(window.innerHeight);
 
 
-const homeBtn = document.getElementById('homeBtn');
-homeBtn.addEventListener('click', learn);
+// const homeBtn = document.getElementById('homeBtn');
+// homeBtn.addEventListener('click', learn);
 
 function learn() {
-    window.scroll({
-        top : window.innerHeight * 2.25,
-        left : 0,
-        behavior : 'smooth',
-    });
+    if (screen.width > 600) {
+        window.scroll({
+            top : window.innerHeight * 2.25,
+            left : 0,
+            behavior : 'smooth',
+        });
+    } else {
+        location.href = "HTML/road.html";
+    }
+    
 }
 
 function roads() {
@@ -72,44 +88,69 @@ routeBtn.addEventListener('mouseout', function() {
     innerBtn.classList.remove('btnClicked');
 })
 
+//other parallax
 
-//function parallax() {
-//     const horizontal = document.getElementById('horizontal');
-//     const vertical = document.getElementById('vertical');
-//     const videoBG = document.getElementById('background');
-//     const video = document.getElementById('backgroundVideo');
-//     const postVideoCard = document.getElementById('postVideo');
-//     const overlay = document.getElementById('overlay');
-//     const introBox = document.getElementById('introBox');
-    
-    
-//     if(window.pageYOffset < window.innerWidth * 2.5) {
-//         horizontal.style.left = -1.2 * window.pageYOffset;
-//         videoBG.style.left = -1 * window.pageYOffset / 5;
-//         overlay.style.left = -1 * window.pageYOffset / 5;
-//         videoBG.style.top = 0;
-//         postVideoCard.style.marginLeft = window.innerWidth * 2.5 - window.pageYOffset;
-//     } else if(window.pageYOffset > window.innerWidth * 2) {
-//         videoBG.style.left = window.innerWidth * -2.5/5;
-//         horizontal.style.left = window.innerWidth * -2.5 * 1.2;
-//         videoBG.style.top = window.pageYOffset - window.innerWidth * 2.5;
-//         postVideoCard.style.marginLeft = window.innerWidth * 2 - window.pageYOffset * 1.75;
-//     }
+const parallaxItems = document.querySelectorAll(".parallax");
 
-//     if(window.scrollY > window.innerWidth / 1.2) {
-//         video.style.marginTop = -window.innerWidth / 8;
-//         video.style.width = window.innerWidth * 1.7;
-//         titleText.classList.add('fade');
-//         logo.classList.add('rotate');
-//         introBox.style.left = -1 * window.innerWidth;
-//     } else {
-//         video.style.marginTop = 0;
-//         video.style.width = window.innerWidth;
-//         titleText.classList.remove('fade');
-//         logo.classList.remove('rotate');
-//         introBox.style.left = window.innerWidth / 2;
-//     }
+let x = 0, y = 0;
 
-// }
+document.querySelector('main').addEventListener('mousemove', (e) => {
+     x = e.clientX - window.innerWidth / 2;
+     y = e.clientY - window.innerHeight / 2;
 
-// window.addEventListener('scroll', parallax);
+
+     parallaxItems.forEach((el) => {
+          let speedx = el.dataset.speedx;
+          let speedy = el.dataset.speedy;
+          let z = el.dataset.speedy / el.dataset.speedx;
+
+          el.style.transform = `translateX(calc(-50% + ${-x * speedx}px)) translateY(calc(-50% + ${y * speedy}px)) perspective(2500px) translateZ(${z}px)`;
+          // console.log(speedx);
+     });
+     // console.log(x, y);
+})
+
+document.querySelector('#menu').addEventListener('mousemove', (e) => {
+     x = e.clientX - window.innerWidth / 2;
+     y = e.clientY - window.innerHeight / 2;
+
+
+     parallaxItems.forEach((el) => {
+          let speedx = el.dataset.speedx;
+          let speedy = el.dataset.speedy;
+          let z = el.dataset.speedy / el.dataset.speedx;
+
+          el.style.transform = `translateX(calc(-50% + ${-x * speedx}px)) translateY(calc(-50% + ${y * speedy}px)) perspective(2500px) translateZ(${z}px)`;
+          // console.log(speedx);
+     });
+     // console.log(x, y);
+})
+
+let timeline = gsap.timeline();
+
+
+Array.from(parallaxItems).filter((el) => !el.classList.contains("text")).forEach((el) => {
+     timeline.from(
+          el, {
+               top : `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+               duration : 3.5,
+               ease : "power3.out",
+          },
+          "4.5"
+     );
+});
+
+const descendBtn = document.getElementById('descendBtn');
+
+descendBtn.addEventListener('click', descend);
+
+function descend() {
+     parallaxItems.forEach((el) => {
+          el.style.top = "-150%";
+          el.style.transition = `${2.5 - +el.dataset.speedy * 10}s ease-in-out`;
+          el.style.transitionDelay = "0.3s";
+          console.log(el.dataset.speedy);
+     });
+     document.getElementById('transitionClouds').style.top = "-450%";
+     setTimeout(() => {location.href = 'HTML/road.html'}, 1400);
+}
